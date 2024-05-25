114e2efe02eb7cd4a276b6707bba7cb0252f297f
itsrahulchaudhary <rahulcy49@gmail.com>
2024-05-25 18:05:22 +0530

    test cases comment

--- src/test/java/com/fundoo/app/FundooNotes/FundooNotesApplicationTests.java
+++ src/test/java/com/fundoo/app/FundooNotes/FundooNotesApplicationTests.java
@@ -1,13 +1,13 @@
 package com.fundoo.app.FundooNotes;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FundooNotesApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FundooNotesApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
