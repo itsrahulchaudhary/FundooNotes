c09465d79a14175c85368386139a84d06b1e5439
itsrahulchaudhary <rahulcy49@gmail.com>
2025-04-17 12:09:40 +0530

    added master.

--- src/main/java/com/fundoo/app/user/controller/UserController.java
+++ src/main/java/com/fundoo/app/user/controller/UserController.java
@@ -39,6 +39,7 @@ public class UserController {
 
 	@PostMapping("/login")
 	public ResponseEntity<ResponseToken> login(@RequestBody LoginDTO loginDTO) {
+		System.out.println("master login");
 		ResponseToken statusResponse = userServices.login(loginDTO);
 		return new ResponseEntity<ResponseToken>(statusResponse, HttpStatus.OK);
 	}
